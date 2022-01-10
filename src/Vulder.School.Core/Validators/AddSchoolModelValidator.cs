a9146682b2432f5e0d61f76476d5f6ab760d0746
MRmlik12 <mrmlik12sup@gmail.com>
2022-01-10 19:44:22 +0100

    Add school model validator

---
+++ src/Vulder.School.Core/Validators/AddSchoolModelValidator.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+using Vulder.School.Core.Models;
+
+namespace Vulder.School.Core.Validators;
+
+public class AddSchoolModelValidator : AbstractValidator<AddSchoolModel>
+{
+    public AddSchoolModelValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.TimetableUrl)
+            .NotEqual(x => x.SchoolUrl)
+            .Must(ValidateUri).When(x => !string.IsNullOrEmpty(x.SchoolUrl));
+        
+        RuleFor(x => x.SchoolUrl)
+            .NotEqual(x => x.TimetableUrl)
+            .Must(ValidateUri).When(x => !string.IsNullOrEmpty(x.SchoolUrl));
+    }
+
+    private static bool ValidateUri(string url)
+        => Uri.TryCreate(url, UriKind.Absolute, out _);
+}
