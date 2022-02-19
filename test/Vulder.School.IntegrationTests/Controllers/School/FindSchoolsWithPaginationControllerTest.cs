c5442540d295aa152541badb3e958f8d53ddd542
MRmlik12 <mrmlik12sup@gmail.com>
2022-02-19 14:39:22 +0100

    Add find schools with pagination controller

--- test/Vulder.School.IntegrationTests/Controllers/School/FindSchoolsWithPaginationControllerTest.cs
+++ test/Vulder.School.IntegrationTests/Controllers/School/FindSchoolsWithPaginationControllerTest.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Text.Json;
+using Newtonsoft.Json;
 using Vulder.School.Core.Models;
 using Vulder.School.Core.ProjectAggregate.School.Dtos;
 using Vulder.School.IntegrationTests.Fixtures;
@@ -11,26 +11,29 @@ using Xunit;
 
 namespace Vulder.School.IntegrationTests.Controllers.School;
 
-public class FindSchoolsControllerTest
+public class FindSchoolsWithPaginationControllerTest
 {
     [Fact]
-    public async void GET_Responds_1_Model()
+    public async void GET_Responds_200_StatusCode()
     {
         var body = new AddSchoolModel
         {
-            Name = "SP 2 w Warszawie",
+            Name = "SP 3 w Warszawie",
             SchoolUrl = "http://example.com",
             TimetableUrl = "http://example.com/timetable"
         };
 
         await using var application = new WebServerFactory();
         using var client = application.CreateClient();
-        var httpContent = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+        var httpContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
         await client.PostAsync("/school/AddSchool", httpContent);
-        using var findResponse = await client.GetAsync("school/FindSchools?input=SP");
+
+        using var findResponse = await client.GetAsync("school/FindSchoolsWithPagination?input=SP&page=1");
+        var serializedResponse =
+            JsonConvert.DeserializeObject<SchoolsDto>(await findResponse.Content.ReadAsStringAsync()) ??
+            throw new InvalidOperationException();
 
         Assert.Equal(HttpStatusCode.OK, findResponse.StatusCode);
-        Assert.Single(JsonSerializer.Deserialize<List<SchoolItemDto>>(await findResponse.Content.ReadAsStreamAsync()) ??
-                      throw new InvalidOperationException());
+        Assert.Single(serializedResponse.Schools!.Where(x => x.Name == body.Name));
     }
 }
