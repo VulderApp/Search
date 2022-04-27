f58b872ff6eb0bda4785526b5d7d46e67687f6a6
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-27 08:48:17 +0100

    Return 204 status code when list is empty

--- src/Vulder.School.Infrastructure/AutoMapper/AutoMapperProfile.cs
+++ src/Vulder.School.Infrastructure/AutoMapper/AutoMapperProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Vulder.School.Core.Models;
+using Vulder.School.Core.ProjectAggregate.School.Dtos;
 
 namespace Vulder.School.Infrastructure.AutoMapper;
 
@@ -9,5 +10,6 @@ public class AutoMapperProfile : Profile
     {
         CreateMap<AddSchoolModel, Core.ProjectAggregate.School.School>();
         CreateMap<UpdateSchoolModel, Core.ProjectAggregate.School.School>();
+        CreateMap<Core.ProjectAggregate.School.School, SchoolItemDto>();
     }
 }
